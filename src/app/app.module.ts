2c5e4117a081491b5425a98841814ce071c4eb30
fraagu10 <aguilarfranco1092@protonmail.com>
2023-01-24 11:53:32 -0600

    Mission Planning Dashboard structure foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { CrewComponent } from './crew/crew.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EquipmentComponent,
+    CrewComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
